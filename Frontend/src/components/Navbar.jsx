51767a0efb3827fa0b4282551fad4f47fd5bb930
Haribaskar <baskarh54@gamil.com>
2025-05-31 11:16:36 +0530

    logo

--- Frontend/src/components/Navbar.jsx
+++ Frontend/src/components/Navbar.jsx
@@ -20,7 +20,7 @@ export default function Navbar() {
       <div className="max-w-screen-xl mx-auto px-6 py-4 flex items-center justify-between">
         {/* Logo */}
         <div className="flex items-center space-x-2">
-          <img src={`/asstes/Logo.png`} alt="Skyman Investments Logo" className="h-14 w-auto" />
+          <img src='logos/Logo.png' alt="Skyman Investments Logo" className="h-14 w-auto" />
           <div>
             <Link
               to="/"
